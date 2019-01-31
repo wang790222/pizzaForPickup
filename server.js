c8af9788dbd81e0e8598c96a884251b76e722798
wang790222 <wang790222@gmail.com>
2019-01-31 19:39:36 +0000

    Remove dependency of cookie-parser

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const PORT        = process.env.PORT || 8080;
 const ENV         = process.env.ENV || "development";
 const express     = require("express");
 const bodyParser  = require("body-parser");
-const cookieParser = require('cookie-parser');
 const sass        = require("node-sass-middleware");
 const app         = express();
 
@@ -21,7 +20,6 @@ const midtermRoutes = require("./routes/midterm");
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-app.use(cookieParser());
 app.use(morgan('dev'));
 
 // Log knex SQL queries to STDOUT as well
