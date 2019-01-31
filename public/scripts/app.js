b88a8b2ea3365c5f62f8bb3d07bec8ec95bfa8ad
trabnett <timrabnett@gmail.com>
2019-01-31 19:17:40 +0000

    fixed bootstrap conflict with jquerey

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,22 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
-});
+
+$( document ).ready(function() {
+
+  $(".btn-primary").on('click', function(){
+    localStorage.setItem("crust", "thin");
+    console.log(localStorage);
+  });
+
+
+
+    $.ajax({
+      method: "GET",
+      url: "/api"
+    }).done((items) => {
+      for(item of items) {
+        console.log(item);
+      }
+    });;
+
+
+
+})
--- routes/midterm.js
+++ routes/midterm.js
@@ -8,7 +8,7 @@ module.exports = (knex) => {
   router.get("/", (req, res) => {
     knex
       .select("*")
-      .from("users")
+      .from("crust")
       .then((results) => {
         res.json(results);
     });
