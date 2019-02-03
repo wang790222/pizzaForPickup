b6e359c46a70cbdce56a16645ca857be4a12b02a
wang790222 <wang790222@gmail.com>
2019-02-03 21:11:54 +0000

    Confirm & Pickup Btn

--- db/seeds/a_customer.js
+++ db/seeds/a_customer.js
@@ -1,31 +1,5 @@
 exports.seed = function(knex, Promise) {
   return knex('customer').del()
     .then(function () {
-      return Promise.all([
-        knex('customer').insert(
-          {
-            name: "Tim",
-            phone: "416456789",
-            post_code: "M6V"
-          }),
-          knex('customer').insert(
-          {
-            name: "Rachel",
-            phone: "904456789",
-            post_code: "M2T"
-          }),
-          knex('customer').insert(
-          {
-            name: "Yu-Ning",
-            phone: "123456789",
-            post_code: "MV4"
-          }),
-          knex('customer').insert(
-          {
-            name: "Hilary",
-            phone: "245456789",
-            post_code: "B2X"
-          })
-      ]);
     });
 };
--- db/seeds/c_feedback.js
+++ db/seeds/c_feedback.js
@@ -1,21 +1,4 @@
 exports.seed = function(knex, Promise) {
   return knex('feedback').del()
-    .then(function () {
-      return Promise.all([
-        knex('feedback').insert(
-          {
-            order_id: 1,
-            time_feedback: "2019-01-30 0:01:23",
-            rating_star: 5,
-            rating_text: "Good Job."
-          }),
-        knex('feedback').insert(
-          {
-            order_id: 2,
-            time_feedback: "2019-01-31 0:10:23",
-            rating_star: 4,
-            rating_text: "Great, but more cheese."
-          })
-      ]);
-    });
+    .then(function () {});
 };
