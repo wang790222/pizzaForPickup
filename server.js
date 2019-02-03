e5906078e740acf7d481e303826a9c3003bfbbb7
wang790222 <wang790222@gmail.com>
2019-02-03 22:19:16 +0000

    Comment Out Sending Text

--- server.js
+++ server.js
@@ -270,7 +270,7 @@ app.post("/customer", (req, res) => {
       .into("customer")
       .then(function (id) {
         cb(parseInt(id));
-
+/*
       client.messages.create({
         body: 'New Pizza Order!',
         to: '+15149437993',   //Tim's number
@@ -279,6 +279,7 @@ app.post("/customer", (req, res) => {
         .then((message) => console.log(message.sid))
         .done();
       });
+*/
     });
 
     res.status(200);
@@ -309,6 +310,8 @@ app.post("/confirm/orders", (req, res) => {
     )
     .then(function(values) {
       console.log("Confirm.");
+
+      /*
       client.messages.create({
             body: `Your Order Is Confirmed!`,
             to: '+16476731359',   //Yu-Ning's number
@@ -316,6 +319,7 @@ app.post("/confirm/orders", (req, res) => {
       })
       .then((message) => console.log(message.sid))
       .done();
+      */
 
       res.redirect('back');
     });
